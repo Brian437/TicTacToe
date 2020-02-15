4170af19ce97f94e7086d4c6204ea8698bc664d3
Brian437 <brianchaves7@gmail.com>
2020-02-15 13:34:42 -0500

    move files

---
+++ bChaves Assinment 3/FormGame.cs
@@ -0,0 +1,545 @@
+﻿/* Brian Chaves
+ * October 18
+ * Assinment 3
+ * 15 Squares
+ * Game Form
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace bChaves_Assinment_3
+{
+    public partial class FormGame : Form
+    {
+        int intNumberOfRows = 0;
+        int intNumberOfColumns = 0;
+        int[,] intSquareValue;
+
+        const int int_DEFALT_NUMBER_OF_COLUMNS = 4;
+        const int int_DEFALT_NUMBER_OF_ROWS = 4;
+        const int int_START_LEFT = 20;
+        const int int_START_TOP = 40;
+        const int int_LENGTH = 100;
+        const int int_RIGHT_SPACE = 20;
+        const int int_BOTTOM_SPACE = 40;
+        const int int_FONT_SIZE = 30;
+        const decimal dec_MOVE_COUNTER_TO_NUMBER_OF_SQUARE_RATIO = 25m;
+
+        Label[,] lblSquare;
+
+        /// <summary>
+        /// Square click event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void lblSquare_Click(object sender, EventArgs e)
+        {
+            int intColumn = 0;
+            int intRow = 0;
+            int intEmptyColumn;
+            int intEmptyRow;
+            
+            for (int x = 0; x < intNumberOfColumns; x++)
+            {
+                for (int y = 0; y < intNumberOfRows; y++)
+                {
+                    if (sender.Equals(lblSquare[x, y]))
+                    {
+                        intColumn = x;
+                        intRow = y;
+                    }
+                }
+            }
+
+            getEmptySquare(out intEmptyColumn, out intEmptyRow);
+
+            if (intColumn == intEmptyColumn ^ intRow == intEmptyRow)
+            {
+
+                if (intRow > intEmptyRow)//Move Down
+                {
+                    for (int y = intEmptyRow; y < intRow; y++)
+                    {
+                        intSquareValue[intColumn, y] = intSquareValue[intColumn, y + 1];
+                    }
+                }
+                else if (intRow < intEmptyRow)//Move Up
+                {
+                    for (int y = intEmptyRow; y > intRow; y--)
+                    {
+                        intSquareValue[intColumn, y] = intSquareValue[intColumn, y - 1];
+                    }
+                }
+                else if (intColumn > intEmptyColumn)//Move Left
+                {
+                    for (int x = intEmptyColumn; x < intColumn; x++)
+                    {
+                        intSquareValue[x, intRow] = intSquareValue[x + 1, intRow];
+                    }
+                }
+                else if (intColumn < intEmptyColumn)//Move Right
+                {
+                    for (int x = intEmptyColumn; x > intColumn; x--)
+                    {
+                        intSquareValue[x, intRow] = intSquareValue[x - 1, intRow];
+                    }
+                }
+                intSquareValue[intColumn, intRow] = 0;
+                updateGame();
+            }
+
+        }
+
+        public FormGame()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// Form load event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void FormGame_Load(object sender, EventArgs e)
+        {
+            CallIntroForm();
+        }
+        /// <summary>
+        /// Gets the intro form
+        /// </summary>
+        private void CallIntroForm()
+        {
+            this.Hide();
+
+            string strGameMode = "";
+            FormIntro formIntro = new FormIntro();
+            formIntro.ShowDialog();
+            strGameMode = formIntro.StrGameMode;
+            formIntro.Dispose();
+
+            if (strGameMode == "original")
+            {
+                intNumberOfRows = int_DEFALT_NUMBER_OF_ROWS;
+                intNumberOfColumns = int_DEFALT_NUMBER_OF_COLUMNS;
+                createGame(true);
+            }
+            else if (strGameMode == "custom")
+            {
+                DlgCustom dlgCustom = new DlgCustom();
+                dlgCustom.ShowDialog();
+                intNumberOfColumns = dlgCustom.IntNumberOfColumns;
+                intNumberOfRows = dlgCustom.IntNumberOfRows;
+                dlgCustom.Dispose();
+
+                if (intNumberOfColumns == -1)
+                {
+                    CallIntroForm();
+                }
+                else
+                {
+                    createGame(true);
+                }
+            }
+            else if (strGameMode == "load game")
+            {
+                DialogResult result = openFileDialog.ShowDialog();
+                switch (result)
+                {
+                    case DialogResult.Abort:
+                        break;
+                    case DialogResult.Cancel://Cancels load
+                        CallIntroForm();
+                        break;
+                    case DialogResult.Ignore:
+                        break;
+                    case DialogResult.No:
+                        break;
+                    case DialogResult.None:
+                        break;
+                    case DialogResult.OK://finds load file
+                        string strFileName = openFileDialog.FileName;
+                        doLoad(strFileName);
+                        break;
+                    case DialogResult.Retry:
+                        break;
+                    case DialogResult.Yes:
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else if (strGameMode == "quit")
+            {
+                this.Close();
+            }
+        }
+        /// <summary>
+        /// Loads the game
+        /// </summary>
+        /// <param name="strFileName">File Name</param>
+        private void doLoad(string strFileName)
+        {
+            try
+            {
+                StreamReader reader = new StreamReader(strFileName);
+                intNumberOfColumns = int.Parse(reader.ReadLine());
+                intNumberOfRows = int.Parse(reader.ReadLine());
+
+                intSquareValue = new int[intNumberOfColumns, intNumberOfRows];
+
+                for (int x = 0; x < intNumberOfColumns; x++)
+                {
+                    for (int y = 0; y < intNumberOfRows; y++)
+                    {
+                        intSquareValue[x, y] = int.Parse(reader.ReadLine());
+                    }
+                }
+                createGame(false);
+            }
+            catch
+            {
+                MessageBox.Show("Failed to load game", this.Text);
+                CallIntroForm();
+            }
+        }
+        /// <summary>
+        /// Creates the game
+        /// </summary>
+        /// <param name="newGame">
+        /// Is this a new game or a loaded game.
+        /// Returns true if it is a new game
+        /// </param>
+        private void createGame(bool newGame)
+        {
+            lblSquare = new Label[intNumberOfColumns, intNumberOfRows];
+            if (newGame)
+            {
+                intSquareValue = new int[intNumberOfColumns, intNumberOfRows];
+            }
+
+            for (int x = 0; x < intNumberOfColumns; x++)
+            {
+                for (int y = 0; y < intNumberOfRows; y++)
+                {
+                    if (newGame)
+                    {
+                        if (x == (intNumberOfColumns - 1) && y == (intNumberOfRows - 1))
+                        {
+                            intSquareValue[x, y] = 0;
+                        }
+                        else
+                        {
+                            intSquareValue[x, y] = (x + 1) + y * intNumberOfColumns;
+                        }
+                    }
+
+                    lblSquare[x, y] = new Label();
+                    lblSquare[x, y].Left = int_START_LEFT + int_LENGTH * x;
+                    lblSquare[x, y].Top = int_START_TOP + int_LENGTH * y;
+                    lblSquare[x, y].Width = int_LENGTH;
+                    lblSquare[x, y].Height = int_LENGTH;
+                    lblSquare[x, y].BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+                    lblSquare[x, y].Font = new Font("Microsoft Sans Serif", int_FONT_SIZE);
+
+                    this.Controls.Add(lblSquare[x, y]);
+                    lblSquare[x, y].Click += new EventHandler(lblSquare_Click);
+                }
+            }
+
+            if (newGame)
+            {
+                randomize();
+            }
+
+            updateGame();
+            this.Width = int_LENGTH * intNumberOfColumns + int_START_LEFT + int_RIGHT_SPACE;
+            this.Height = int_LENGTH * intNumberOfRows + int_START_TOP + int_BOTTOM_SPACE;
+            this.CenterToScreen();
+            this.Show();
+            this.Activate();
+        }
+        /// <summary>
+        /// Exit menu
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void menuExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        /// <summary>
+        /// Main Menu button
+        /// sends user to main menu
+        /// any unsaved game will be lost
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void menuMain_Click(object sender, EventArgs e)
+        {
+            DialogResult result;
+            result = MessageBox.Show("Are you sure you want to quit to main menu?",
+                this.Text, MessageBoxButtons.YesNo);
+            if (result == DialogResult.Yes)
+            {
+                clearGame();
+                CallIntroForm();
+            }
+
+        }
+
+        /// <summary>
+        /// Clears the game
+        /// </summary>
+        private void clearGame()
+        {
+            for (int x = 0; x < intNumberOfColumns; x++)
+            {
+                for (int y = 0; y < intNumberOfRows; y++)
+                {
+                    lblSquare[x, y].Dispose();
+                }
+            }
+        }
+        /// <summary>
+        /// Form close event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void FormGame_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (e.CloseReason == CloseReason.ApplicationExitCall)
+            {
+                DialogResult result;
+                result = MessageBox.Show("Are you sure you want to Exit game?",
+                    this.Text, MessageBoxButtons.YesNo);
+                if (result == DialogResult.No)
+                {
+                    e.Cancel = true;
+                }
+            }
+        }
+        /// <summary>
+        /// Saves the game
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void menuSave_Click(object sender, EventArgs e)
+        {
+            DialogResult result = saveFileDialog.ShowDialog();
+            switch (result)
+            {
+                case DialogResult.Abort:
+                    break;
+                case DialogResult.Cancel:
+                    break;
+                case DialogResult.Ignore:
+                    break;
+                case DialogResult.No:
+                    break;
+                case DialogResult.None:
+                    break;
+                case DialogResult.OK:
+                    string strFileName = saveFileDialog.FileName;
+                    doSave(strFileName);
+                    break;
+                case DialogResult.Retry:
+                    break;
+                case DialogResult.Yes:
+                    break;
+                default:
+                    break;
+            }
+        }
+        /// <summary>
+        /// Saves the game
+        /// </summary>
+        /// <param name="strFileName">File Name</param>
+        void doSave(String strFileName)
+        {
+            StreamWriter writer = new StreamWriter(strFileName);
+            writer.WriteLine(intNumberOfColumns);
+            writer.WriteLine(intNumberOfRows);
+
+            for (int x = 0; x < intNumberOfColumns; x++)
+            {
+                for (int y = 0; y < intNumberOfRows; y++)
+                {
+                    writer.WriteLine(intSquareValue[x, y]);
+                }
+            }
+
+            writer.Close();
+
+            MessageBox.Show("Game Saved", this.Text);
+        }
+        /// <summary>
+        /// Updates squares
+        /// </summary>
+        private void updateGame()
+        {
+            for (int x = 0; x < intNumberOfColumns; x++)
+            {
+                for (int y = 0; y < intNumberOfRows; y++)
+                {
+                    if (intSquareValue[x, y] == 0)
+                    {
+                        lblSquare[x, y].Text = "";
+                    }
+                    else
+                    {
+                        lblSquare[x, y].Text = intSquareValue[x, y].ToString();
+                    }
+                }
+            }
+
+            if (checkForWinner())
+            {
+                MessageBox.Show("You win", this.Text);
+                DialogResult result = MessageBox.Show("Play New Game?", this.Text, MessageBoxButtons.YesNo);
+                if (result == DialogResult.Yes)
+                {
+                    clearGame();
+                    CallIntroForm();
+                }
+            }
+        }
+        /// <summary>
+        /// Gets the position for the empty square
+        /// </summary>
+        /// <param name="intColumn">empty square column</param>
+        /// <param name="intRow">empty square row</param>
+        private void getEmptySquare(out int intColumn, out int intRow)
+        {
+            intColumn = 0;
+            intRow = 0;
+
+            for (int x = 0; x < intNumberOfColumns; x++)
+            {
+                for (int y = 0; y < intNumberOfRows; y++)
+                {
+                    if (intSquareValue[x, y] == 0)
+                    {
+                        intColumn = x;
+                        intRow = y;
+                    }
+                }
+            }
+        }
+        /// <summary>
+        /// Randomize Meathod will move the square randomly
+        /// to generate a new game
+        /// </summary>
+        private void randomize()
+        {
+            int intNumberOfMovesRemain = (int)((decimal)intNumberOfColumns *
+                (decimal)intNumberOfRows * dec_MOVE_COUNTER_TO_NUMBER_OF_SQUARE_RATIO);
+
+            Random random = new Random();
+            int idMoveLoation = 0;
+
+            const int int_MIN_VALUE = 1;
+            const int int_MAX_VALUE = 4;
+
+            int intEmptyColumn;
+            int intEmptyRow;
+            getEmptySquare(out intEmptyColumn, out intEmptyRow);
+
+            while (intNumberOfMovesRemain > 0)
+            {
+                idMoveLoation = random.Next(int_MIN_VALUE, int_MAX_VALUE + 1);
+                if (idMoveLoation == (int)move.up)
+                {
+                    if (intEmptyRow > 0)
+                    {
+                        intSquareValue[intEmptyColumn, intEmptyRow] =
+                            intSquareValue[intEmptyColumn, intEmptyRow - 1];
+                        intSquareValue[intEmptyColumn, intEmptyRow - 1] = 0;
+                        intEmptyRow--;
+                        intNumberOfMovesRemain--;
+                    }
+                }
+                else if (idMoveLoation == (int)move.right)
+                {
+                    if (intEmptyColumn < (intNumberOfColumns - 1))
+                    {
+                        intSquareValue[intEmptyColumn, intEmptyRow] =
+                            intSquareValue[intEmptyColumn + 1, intEmptyRow];
+                        intSquareValue[intEmptyColumn + 1, intEmptyRow] = 0;
+                        intEmptyColumn++;
+                        intNumberOfMovesRemain--;
+                    }
+                }
+                else if (idMoveLoation == (int)move.down)
+                {
+                    if (intEmptyRow < (intNumberOfRows - 1))
+                    {
+                        intSquareValue[intEmptyColumn, intEmptyRow] =
+                            intSquareValue[intEmptyColumn, intEmptyRow + 1];
+                        intSquareValue[intEmptyColumn, intEmptyRow + 1] = 0;
+                        intEmptyRow++;
+                        intNumberOfMovesRemain--;
+                    }
+                }
+                else if (idMoveLoation == (int)move.left)
+                {
+                    if (intEmptyColumn > 0)
+                    {
+                        intSquareValue[intEmptyColumn, intEmptyRow] =
+                            intSquareValue[intEmptyColumn - 1, intEmptyRow];
+                        intSquareValue[intEmptyColumn - 1, intEmptyRow] = 0;
+                        intEmptyColumn--;
+                        intNumberOfMovesRemain--;
+                    }
+                }
+            }
+            updateGame();
+
+        }
+        /// <summary>
+        /// a meathod that checks if user has successfully completed the puzzle or not
+        /// </summary>
+        /// <returns></returns>
+        private bool checkForWinner()
+        {
+            bool winGame = true;
+            for (int x = 0; x < intNumberOfColumns; x++)
+            {
+                for (int y = 0; y < intNumberOfRows; y++)
+                {
+                    if (!(x == (intNumberOfColumns - 1) && y == (intNumberOfRows - 1)))
+                    {
+                        if(intSquareValue[x,y] != (x + 1) + y * intNumberOfColumns)
+                        {
+                            winGame=false;
+                        }
+                    }
+                }
+            }
+            return winGame;
+        }
+
+        enum move
+        {
+            up = 1,
+            right = 2,
+            down = 3,
+            left = 4
+        }
+        /// <summary>
+        /// About Menu Clicked
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void menuAbout_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Created by Brian L. Chaves \r\n"
+                + "October 2012", this.Text);
+        }
+    }
+}
---
+++ bChaves Assinment 3/FormIntro.designer.cs
@@ -0,0 +1,144 @@
+﻿namespace bChaves_Assinment_3
+{
+    partial class FormIntro
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picLogo = new System.Windows.Forms.PictureBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnCustom = new System.Windows.Forms.Button();
+            this.btnOriginal = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picLogo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picLogo
+            // 
+            this.picLogo.Image = global::bChaves_Assinment_3.Properties.Resources.BC_logo;
+            this.picLogo.Location = new System.Drawing.Point(709, 483);
+            this.picLogo.Name = "picLogo";
+            this.picLogo.Size = new System.Drawing.Size(63, 67);
+            this.picLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picLogo.TabIndex = 5;
+            this.picLogo.TabStop = false;
+            this.picLogo.Click += new System.EventHandler(this.picLogo_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.AutoSize = true;
+            this.btnExit.BackColor = System.Drawing.Color.White;
+            this.btnExit.Image = global::bChaves_Assinment_3.Properties.Resources.Exit_Game;
+            this.btnExit.Location = new System.Drawing.Point(393, 378);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(203, 105);
+            this.btnExit.TabIndex = 4;
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.AutoSize = true;
+            this.btnLoad.BackColor = System.Drawing.Color.White;
+            this.btnLoad.Image = global::bChaves_Assinment_3.Properties.Resources.Load_Game;
+            this.btnLoad.Location = new System.Drawing.Point(184, 378);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(203, 105);
+            this.btnLoad.TabIndex = 3;
+            this.btnLoad.UseVisualStyleBackColor = false;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // btnCustom
+            // 
+            this.btnCustom.AutoSize = true;
+            this.btnCustom.BackColor = System.Drawing.Color.White;
+            this.btnCustom.Image = global::bChaves_Assinment_3.Properties.Resources.Custom_Game;
+            this.btnCustom.Location = new System.Drawing.Point(184, 262);
+            this.btnCustom.Name = "btnCustom";
+            this.btnCustom.Size = new System.Drawing.Size(412, 110);
+            this.btnCustom.TabIndex = 2;
+            this.btnCustom.UseVisualStyleBackColor = false;
+            this.btnCustom.Click += new System.EventHandler(this.btnCustom_Click);
+            // 
+            // btnOriginal
+            // 
+            this.btnOriginal.AutoSize = true;
+            this.btnOriginal.BackColor = System.Drawing.Color.White;
+            this.btnOriginal.Image = global::bChaves_Assinment_3.Properties.Resources.Original15SquareGame;
+            this.btnOriginal.Location = new System.Drawing.Point(184, 146);
+            this.btnOriginal.Name = "btnOriginal";
+            this.btnOriginal.Size = new System.Drawing.Size(412, 110);
+            this.btnOriginal.TabIndex = 1;
+            this.btnOriginal.UseVisualStyleBackColor = false;
+            this.btnOriginal.Click += new System.EventHandler(this.btnOriginal_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::bChaves_Assinment_3.Properties.Resources._15_Square_Game;
+            this.pictureBox1.Location = new System.Drawing.Point(91, 44);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(590, 96);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // FormIntro
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(784, 562);
+            this.ControlBox = false;
+            this.Controls.Add(this.picLogo);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.btnCustom);
+            this.Controls.Add(this.btnOriginal);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "FormIntro";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "15 Square Game";
+            this.Load += new System.EventHandler(this.FormIntro_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picLogo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnOriginal;
+        private System.Windows.Forms.Button btnCustom;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.PictureBox picLogo;
+    }
+}
